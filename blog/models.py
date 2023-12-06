f14fab5b581417f61a0cba96c2063d4394ef04c0
mitterandassagui <asmitterand@yahoo.fr>
2023-12-06 13:58:03 +0100

    last modified file

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-#from django.db import settings
+from django.conf import settings
 from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
